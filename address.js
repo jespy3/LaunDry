b09dd1dc885c1bebb3f03398dd923a266e759cc3
Jargil Santos <jargil.santos@gmail.com>
2018-05-19 16:41:14 +1200

    Get geo coordinates now works

--- address.js
+++ address.js
@@ -6,7 +6,10 @@ function guessAddress(usertext) {
     var components = "components=country:nz";
     var radius = "radius=200000";
     var types = "types=address";
-    var input = "input=" + usertext;
+    //var input = "input=" + usertext;
+
+    var input = "input=14 westwell rd";
+
     //console.log("Hi!");
     var autocompleteUri = baseUri + autocomplete + "json?" + mapsapikey + "&" + latlong + "&" + components
      + "&" + radius + "&" + types + "&" + input;
@@ -16,29 +19,31 @@ function guessAddress(usertext) {
     var xhr = new XMLHttpRequest();
     xhr.open("GET", autocompleteUri, true);
     xhr.onload = function () {
-        //lookupResults = JSON.parse(xhr.responseText);
-        results = JSON.parse(xhr.responseText);
-        console.log(results);
-
+        lookupResults = JSON.parse(xhr.responseText);
+        //results = JSON.parse(xhr.responseText);
+        //console.log(results);
+        // return lookupResults.predictions[0];
+        //console.log(JSON.stringify(lookupResults))
+        var location = getAddressLocation(lookupResults.predictions[0])
     }
     xhr.send(null);
 
     //console.log(JSON.stringify(lookupResults));
 
-    return lookupResults.predictions[0];
+    
 }
 
-function lookupAddressLocation() {
-    var myaddress = "26 wicklow rd";
+/* function lookupAddressLocation() {
+    
     var prediction = guessAddress(myaddress);
     var location = getAddressLocation(prediction);
-}
+} */
 
 function getAddressLocation(prediction) {
     var placeID = prediction.place_id;
     var mapsapikey = "key=AIzaSyAXAnWVJ3Zjo0lwBw-6fKzvO-w7--W7_U4";
     var baseUri = "https://maps.googleapis.com/maps/api/place/";
-    var detailsUri = baseUri + "details/JSON?" + mapsapikey + "&placeid=" + placeID;
+    var detailsUri = baseUri + "details/json?" + "placeid=" + placeID + "&" + mapsapikey ;
 
     console.log(detailsUri);
     var lookupResults;
@@ -48,12 +53,12 @@ function getAddressLocation(prediction) {
         //lookupResults = JSON.parse(xhr.responseText);
         results = JSON.parse(xhr.responseText);
         console.log(results);
-
+        var locat = results.result.geometry.location;
+        console.log(JSON.stringify(locat));
+    
+        return locat;
     }
     xhr.send(null);
 
-    var locat = results.result.geometry.location;
-    console.log(JSON.stringify(locat));
 
-    return locat;
 }
