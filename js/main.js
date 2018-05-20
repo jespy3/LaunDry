1578d4473b88b2545f12004b9383dacca375c069
Kirsten <meateskirsten@gmail.com>
2018-05-20 12:04:59 +1200

    fixing

--- js/main.js
+++ js/main.js
@@ -181,11 +181,3 @@ function findNextTime(){
     xhr.send(null);
 }
 
-function changeImage(blockOne, blockTwo, imageId){
-    if(blockOne || blockTwo == "Rain"){
-        document.getElementById(mainImage).src = "images/no.gif";
-    }else{
-        document.getElementById(mainImage).src = "images/yes.gif"
-    }
-
-}
